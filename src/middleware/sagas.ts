851164ca51b559b9e806493f9b2d051a3da02d38
sojurner <pykim0591@gmail.com>
2019-01-07 15:59:25 -0700

    Add episodes and locations generator functions

--- src/middleware/sagas.ts
+++ src/middleware/sagas.ts
@@ -3,8 +3,12 @@ import { put } from 'redux-saga/effects';
 import * as call from '../utils/apiCalls';
 
 function* retrieveAndAddData() {
-  const characters = yield call.fetchCharacters();
+  const characters = yield call.fetchData('characters');
   yield put({ type: 'ADD_CHARACTER_SET', payload: characters });
+  const episodes = yield call.fetchData('episodes');
+  yield put({ type: 'ADD_EPISODES', payload: episodes });
+  const locations = yield call.fetchData('locations');
+  yield put({ type: 'ADD_LOCATIONS', payload: locations });
 }
 
 export function* fetchData(type) {
