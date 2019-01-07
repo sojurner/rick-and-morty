4ea2b0c56384ce92bd06d4e40bc7b56953f63eed
sojurner <pykim0591@gmail.com>
2019-01-06 20:50:49 -0700

    Render Navigation and routes components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,23 @@
 import * as React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from './Navigation';
+import Routes from './Routes';
 
-const App = () => {
-  return <h1>Rendered</h1>;
-};
+import '../styles/App.css';
+
+class App extends React.Component<{}, {}> {
+  public render() {
+    const routes: string[] = ['characters', 'locations', 'episodes'];
+
+    return (
+      <Router>
+        <div>
+          <Navigation routes={routes} />
+          <Routes />
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
