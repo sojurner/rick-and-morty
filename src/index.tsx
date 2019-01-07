17d2ec2b732262e1b0626b91cc4362fc700c7ed5
sojurner <pykim0591@gmail.com>
2019-01-06 20:48:05 -0700

    import devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import { fetchData } from './middleware/sagas';
 import App from './components/App';
+import rootReducer from './reducers';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
