acd0f48f70088c392db81a4fb9d839793a0dd8dc
sojurner <pykim0591@gmail.com>
2019-01-07 15:57:07 -0700

    Migrate inital fetch call

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { fetchData } from '../actions';
 import Navigation from './Navigation';
 import Routes from './Routes';
 
 import '../styles/App.css';
 
-class App extends React.Component<{}, {}> {
+interface IAppProps {
+  fetchData: Function;
+}
+class App extends React.Component<IAppProps, {}> {
+  public componentDidMount = () => {
+    const { fetchData } = this.props;
+    fetchData();
+  };
+
   public render() {
     const routes: string[] = ['characters', 'locations', 'episodes'];
 
@@ -20,4 +31,11 @@ class App extends React.Component<{}, {}> {
   }
 }
 
-export default App;
+export const mapDispatchToProps = (dispatch: Function) => ({
+  fetchData: (str: string) => dispatch(fetchData(str))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
