5d57794133f251585a937875d49c30249f9aaa28
sojurner <pykim0591@gmail.com>
2019-01-07 16:00:33 -0700

    implement switch for conditional fetching

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -1,8 +1,23 @@
 import * as scrape from './scrapers';
 
-export const fetchCharacters = async () => {
-  let url: string = 'https://rickandmortyapi.com/api/character/';
-
-  const result: object = await (await fetch(url)).json();
-  return scrape.characters(result);
+export const fetchData = async str => {
+  let url: string;
+  let result: any;
+  switch (str) {
+    case 'characters':
+      url = 'https://rickandmortyapi.com/api/character/';
+      result = scrape.characters(await (await fetch(url)).json());
+      break;
+    case 'episodes':
+      url = 'https://rickandmortyapi.com/api/episode/';
+      result = scrape.episodes(await (await fetch(url)).json());
+      break;
+    case 'locations':
+      url = 'https://rickandmortyapi.com/api/location/';
+      result = scrape.locations(await (await fetch(url)).json());
+      break;
+    default:
+      return;
+  }
+  return result;
 };
