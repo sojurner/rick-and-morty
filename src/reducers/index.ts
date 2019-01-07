5fb52a55015f3525fd08845d967440c1027b0497
sojurner <pykim0591@gmail.com>
2019-01-07 15:59:48 -0700

    Add locations, episodes and pageView to store

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import { characterReducer } from './character_reducer';
+import { episodeReducer } from './episode_reducer';
+import { locationReducer } from './location_reducer';
+import { pageReducer } from './page_reducer';
 
 const rootReducer = combineReducers({
-  characters: characterReducer
+  characters: characterReducer,
+  episodes: episodeReducer,
+  locations: locationReducer,
+  pageView: pageReducer
 });
 
 export default rootReducer;
