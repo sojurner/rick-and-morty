95be2c68e8459064a18756dd39445c9eae535b06
sojurner <pykim0591@gmail.com>
2019-01-06 20:50:26 -0700

    Create routes asset file

---
+++ src/assets/routes.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    path: '/characters',
+    import_path: './Characters'
+  },
+  {
+    path: '/locations',
+    import_path: './Locations'
+  },
+  {
+    path: '/episodes',
+    import_path: './Episodes'
+  }
+];
