0f0d1ed36813fff6048e2d05ea128b435960e1c3
sojurner <pykim0591@gmail.com>
2019-01-08 16:05:25 -0700

    Create PageView component

--- src/components/PageView.tsx
+++ src/components/PageView.tsx
@@ -1,34 +1,37 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import ContentCard from './ContentCard';
 
-interface ICharactersContainerProps {
-  pageView: string;
+interface IPageViewProps {
   dispatch: Function;
+  history: object;
+  location: object;
+  match: object;
+  matchingPage: object;
+  getPage: Function;
+  pageCount: number;
 }
 
-let store;
+interface IPageViewState {
+  page: number;
+}
 
-const PageView = props => {
-  const { state, match } = props;
-  const category: string = match.path.slice(1);
-  const view: object[] = state[category][state.pageView];
+class PageView extends React.Component<IPageViewProps, IPageViewState> {
+  render() {
+    const { matchingPage, category, pageView, pageCount } = this.props;
+    return (
+      <section>
+        {matchingPage.map(item => {
+          return <ContentCard item={item} type={category} />;
+        })}
+        <div />
+      </section>
+    );
+  }
+}
 
-  return (
-    <section>
-      {view[category].map(item => {
-        return <ContentCard item={item} />;
-      })}
-      <div>
-        <button>Prev</button>
-        <button>Next</button>
-      </div>
-    </section>
-  );
-};
-
-export const mapStateToProps = state => ({
-  state
-});
-
-export default connect(mapStateToProps)(PageView);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PageView);
