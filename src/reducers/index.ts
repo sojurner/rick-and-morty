b87e39e9530db5aecabbdc1db18367a4e693423b
sojurner <pykim0591@gmail.com>
2019-01-08 16:07:48 -0700

    Add page count to store

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,13 +2,14 @@ import { combineReducers } from 'redux';
 import { characterReducer } from './character_reducer';
 import { episodeReducer } from './episode_reducer';
 import { locationReducer } from './location_reducer';
-import { pageReducer } from './page_reducer';
+import { pageReducer, pageCountReducer } from './page_reducer';
 
 const rootReducer = combineReducers({
   characters: characterReducer,
   episodes: episodeReducer,
   locations: locationReducer,
-  pageView: pageReducer
+  pageView: pageReducer,
+  pageCount: pageCountReducer
 });
 
 export default rootReducer;
