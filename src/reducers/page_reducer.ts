68b5af604d7e6a5dd70a673317bb6227d1f7aa9e
sojurner <pykim0591@gmail.com>
2019-01-08 16:08:12 -0700

    Create page reducer

--- src/reducers/page_reducer.ts
+++ src/reducers/page_reducer.ts
@@ -1,9 +1,53 @@
-export const pageReducer = (state: number = 0, action: { type: string }) => {
+type initial = {
+  characters: number;
+  episodes: number;
+  locations: number;
+};
+
+let initialState: initial = {
+  characters: 1,
+  episodes: 1,
+  locations: 1
+};
+
+export const pageReducer = (
+  state = initialState,
+  action: { type: string; page: number }
+) => {
   switch (action.type) {
-    case 'NEXT_PAGE':
-      return (state += 1);
-    case 'PREV_PAGE':
-      return (state -= 1);
+    case 'CHARACTER_PAGE':
+      state.characters = action.page;
+      return state;
+
+    case 'EPISODE_PAGE':
+      state.episodes = action.page;
+      return state;
+
+    case 'LOCATION_PAGE':
+      state.locations = action.page;
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+export const pageCountReducer = (
+  state = {},
+  action: { type: string; payload: number }
+) => {
+  switch (action.type) {
+    case 'SET_CHARACTER_PAGECOUNT':
+      state['characters'] = action.payload;
+      return state;
+
+    case 'SET_EPISODE_PAGECOUNT':
+      state['episodes'] = action.payload;
+      return state;
+
+    case 'SET_LOCATION_PAGECOUNT':
+      state['location'] = action.payload;
+      return state;
     default:
       return state;
   }
