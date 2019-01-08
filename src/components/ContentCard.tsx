c903e8ef64a85be4de29b9e03fc7e46685765046
sojurner <pykim0591@gmail.com>
2019-01-08 16:04:34 -0700

    Create ContentCard component

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react';
 
 interface IContentCardProps {
-  item: object;
+  item: {
+    episode: string[];
+    id: 2;
+    image: string;
+    name: string;
+    origin: { name: string; url: string };
+    species: string;
+  };
+  type: string;
 }
 
 const ContentCard = (props: IContentCardProps) => {
-  return <h1>CARD</h1>;
+  const { item, type } = props;
+
+  switch (type) {
+    case 'characters':
+      return (
+        <div>
+          <h1>{item.name}</h1>
+          <img src={item.image} alt="Character image" />
+        </div>
+      );
+    case 'locations':
+      return <h1>{item.name}</h1>;
+  }
 };
 
 export default ContentCard;
