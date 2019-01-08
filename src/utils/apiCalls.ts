1d7ac70c90f65f23ff3a999e1231a12565f9c5f2
sojurner <pykim0591@gmail.com>
2019-01-08 16:08:27 -0700

    Add nextpage fetch call

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -21,3 +21,23 @@ export const fetchData = async str => {
   }
   return result;
 };
+
+export const fetchNextPage = async (str: string, page: number) => {
+  const endpoint: string = str.substring(0, str.length - 1);
+  const url: string = `https://rickandmortyapi.com/api/${endpoint}/?page=${page}`;
+  let result: object;
+  switch (str) {
+    case 'characters':
+      result = scrape.characters(await (await fetch(url)).json());
+      break;
+    case 'locations':
+      result = scrape.locations(await (await fetch(url)).json());
+      break;
+    case 'episodes':
+      result = scrape.episodes(await (await fetch(url)).json());
+      break;
+    default:
+      return;
+  }
+  return result;
+};
