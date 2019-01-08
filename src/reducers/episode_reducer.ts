8582c5e304f5bf7b9608d99a953a2b2fa228b933
sojurner <pykim0591@gmail.com>
2019-01-08 16:07:34 -0700

    Modify structure of episode state

--- src/reducers/episode_reducer.ts
+++ src/reducers/episode_reducer.ts
@@ -1,13 +1,16 @@
-let initialState: any[] = [];
+let initialState = {};
 
 export const episodeReducer = (
   state = initialState,
-  action: { type: string; payload: any }
+  action: { type: string; payload: any; page: number }
 ) => {
   switch (action.type) {
-    case 'ADD_EPISODES':
-      state.push(action.payload);
-      return initialState;
+    case 'ADD_INITIAL_EPISODES':
+      state[action.page] = action.payload;
+      return state;
+    case 'ADD_EPISODE_SET':
+      state[action.page] = action.payload;
+      return state;
     default:
       return state;
   }
