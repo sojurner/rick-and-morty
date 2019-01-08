f35d8c65edf22a2b8eede4d85c7ab8f82cfa9542
sojurner <pykim0591@gmail.com>
2019-01-08 16:06:02 -0700

    Update Routes to include child paths

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,15 +1,72 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import routesList from '../assets/routes';
+import { connect } from 'react-redux';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import Characters from './Characters';
+import Locations from './Locations';
+import Episodes from './Episodes';
+
 import DynamicImports from './DynamicImports';
 
 const Routes = props => {
-  const routeViews: object[] = routesList.map(route => {
-    const { path, component } = route;
+  const routesList: any[] = [
+    {
+      paths: '/characters/:id',
+      key: 'characters',
+      main: Characters
+    },
+    {
+      paths: '/locations/:id',
+      key: 'locations',
+      main: Locations
+    },
+    {
+      paths: '/episodes/:id',
+      key: 'episodes',
+      main: Episodes
+    }
+  ];
 
-    return <Route path={path} component={component} />;
+  const mainRoutes = routesList.map((routes, index) => {
+    const { key, main } = routes;
+    return <Route path={`/${key}`} component={main} />;
   });
-  return <Switch>{routeViews}</Switch>;
+
+  const routeViews: object[] = routesList.map((routes, index) => {
+    const { paths, children, key } = routes;
+    const route = (
+      <Route
+        key={`route-${index}`}
+        path={paths}
+        render={({ match }) => {
+          const id = match.params.id;
+          const matchingPage = props.state[key][id];
+          return (
+            <DynamicImports load={() => import('./PageView')}>
+              {Component => {
+                return !Component ? (
+                  <h1>Loading...</h1>
+                ) : (
+                  <Component category={key} matchingPage={matchingPage} />
+                );
+              }}
+            </DynamicImports>
+          );
+        }}
+      />
+    );
+    console.log(route);
+    return route;
+  });
+  return (
+    <div>
+      {mainRoutes}
+      <Switch>{routeViews}</Switch>
+    </div>
+  );
 };
 
-export default Routes;
+const mapStateToProps = (state: object) => ({
+  state
+});
+
+export default withRouter(connect(mapStateToProps)(Routes));
