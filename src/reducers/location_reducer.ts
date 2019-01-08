b02f1e36a4b5d2f6b03dda047d189fca8143af7d
sojurner <pykim0591@gmail.com>
2019-01-08 16:07:58 -0700

    Change structure of location state

--- src/reducers/location_reducer.ts
+++ src/reducers/location_reducer.ts
@@ -1,13 +1,16 @@
-let initialState: any[] = [];
+let initialState = {};
 
 export const locationReducer = (
   state = initialState,
-  action: { type: string; payload: any }
+  action: { type: string; payload: any; page: number }
 ) => {
   switch (action.type) {
-    case 'ADD_LOCATIONS':
-      state.push(action.payload);
-      return initialState;
+    case 'ADD_INITIAL_LOCATIONS':
+      state[action.page] = action.payload;
+      return state;
+    case 'ADD_LOCATION_SET':
+      state[action.page] = action.payload;
+      return state;
     default:
       return state;
   }
