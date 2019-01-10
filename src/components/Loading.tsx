13dad51a2e94e4e12c8367feaf1b71815835cc6b
sojurner <pykim0591@gmail.com>
2019-01-10 16:57:55 -0700

    Add loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import '../styles/Loading.css';
+
+const Loading = () => (
+  <div className="loading-container">
+    <div className="back-square" />
+    <div className="front-square" />
+  </div>
+);
+
+export default Loading;
