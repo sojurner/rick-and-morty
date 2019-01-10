9c91e0ee73d69b6598f5a40536c9cc74ded36ca5
sojurner <pykim0591@gmail.com>
2019-01-10 16:58:21 -0700

    Attempt custom to routing for navLinks

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,41 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { withPropsOnChange } from 'recompose';
 
 import '../styles/Navigation.css';
 
 interface INavigationProps {
   routes: string[];
+  pageView: { characters: number; episodes: number; locations: number };
 }
 
-const Navigation = (props: INavigationProps) => {
-  const { routes } = props;
+const Navigation = ({ pageView }) => {
+  const routes: { route: string; icon: string }[] = [
+    {
+      route: 'characters',
+      icon:
+        'http://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/512/rick-icon.png'
+    },
+    {
+      route: 'locations',
+      icon:
+        'http://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/512/alien-ship-icon.png'
+    },
+    { route: 'episodes', icon: 'https://i.redd.it/sz1nfssmu4cx.png' }
+  ];
   return (
     <nav className="nav-group">
       {routes.map((link, index) => {
         return (
-          <NavLink key={`link-${index}`} to={`/${link}/1`} className="navlink">
-            {link}
+          <NavLink
+            key={`link-${index}`}
+            exact
+            to={`/${link.route}/${pageView[link.route]}`}
+            className="navlink"
+          >
+            <img src={link.icon} className="rick-icon" />
+            {link.route}
           </NavLink>
         );
       })}
@@ -22,4 +43,8 @@ const Navigation = (props: INavigationProps) => {
   );
 };
 
-export default Navigation;
+const mapStateToProps = state => ({
+  pageView: state.pageView
+});
+
+export default connect(mapStateToProps)(Navigation);
