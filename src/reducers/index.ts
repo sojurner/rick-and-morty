dcbb6439bea0d33f0f8767ea031dc3a511c5b2d4
sojurner <pykim0591@gmail.com>
2019-01-10 17:04:29 -0700

    Add loading to store

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,10 +3,12 @@ import { characterReducer } from './character_reducer';
 import { episodeReducer } from './episode_reducer';
 import { locationReducer } from './location_reducer';
 import { pageReducer, pageCountReducer } from './page_reducer';
+import { loadingReducer } from './loading_reducer';
 
 const rootReducer = combineReducers({
   characters: characterReducer,
   episodes: episodeReducer,
+  loading: loadingReducer,
   locations: locationReducer,
   pageView: pageReducer,
   pageCount: pageCountReducer
