18a72da085b657cb750a330e050c4e97982f80cf
sojurner <pykim0591@gmail.com>
2019-01-11 17:49:49 -0700

    Remember event bubbling/delegation

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -4,31 +4,49 @@ import { withState } from 'recompose';
 
 interface contentCardProps {
   item: { name: string; image?: string };
-  hovered: string;
-  handleHover: Function;
+  handleClick: Function;
+  targetCard: '';
 }
 
 const ContentCard = (props: contentCardProps) => {
   const { name } = props.item;
-  const { hovered, handleHover } = props;
+  const { handleClick, targetCard } = props;
   return (
-    <section
-      onMouseEnter={() => handleHover(name)}
-      onMouseLeave={() => handleHover('')}
-      style={
-        props.item.hasOwnProperty('image')
-          ? { backgroundImage: `url(${props.item.image})` }
-          : { backgroundImage: 'none' }
+    <div
+      className={
+        targetCard === name ? 'card-flip-container-full' : 'card-flip-container'
       }
-      className="content-card"
+      onClick={targetCard !== name ? () => handleClick(name) : () => ''}
     >
-      <h2
-        className={hovered === name ? 'content-name-show' : 'content-name-hide'}
-      >
-        {name}
-      </h2>
-    </section>
+      <div className={targetCard === name ? 'card-itself-full' : 'card-itself'}>
+        {['front-face', 'back-face'].map(item => {
+          return (
+            <section
+              style={
+                props.item.hasOwnProperty('image')
+                  ? { backgroundImage: `url(${props.item.image})` }
+                  : { backgroundImage: 'none' }
+              }
+              className={item}
+            >
+              {targetCard === name && (
+                <i
+                  className="material-icons exit-modal-icon"
+                  onClick={() => handleClick(null)}
+                >
+                  fullscreen_exit
+                </i>
+              )}
+
+              {item === 'back-face' && (
+                <h2 className="content-name"> {name} </h2>
+              )}
+            </section>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
-export default withState('hovered', 'handleHover', '')(ContentCard);
+export default withState('targetCard', 'handleClick', '')(ContentCard);
